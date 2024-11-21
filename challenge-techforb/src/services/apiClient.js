21ce2882a51be3e3db436518ac4fed406365c1de
Leonardo Paz <flajarkin99@hotmail.com>
2024-11-21 05:35:46 -0300

    actualizacion del backend, uso de railway para consumir

--- challenge-techforb/src/services/apiClient.js
+++ challenge-techforb/src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-	baseURL: 'http://localhost:5000',
+	baseURL: 'https://techforb-backend-production.up.railway.app',
 	headers: {
 		'Content-Type': 'application/json',
 	},
